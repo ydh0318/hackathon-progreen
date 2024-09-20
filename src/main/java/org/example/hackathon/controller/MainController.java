9b8850eccdad00da0a39eed8d1cf5c3d09249301
ydh0318 <edwinuo3@gmail.com>
2024-09-20 20:23:41 +0900

    application 파일, thymeleaf 추가

--- src/main/java/org/example/hackathon/controller/MainController.java
+++ src/main/java/org/example/hackathon/controller/MainController.java
@@ -5,13 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-//@RequestMapping("/progreen")
+@RequestMapping("/progreen")
 public class MainController {
     public MainController() {
 
     }
 
-    @GetMapping("/")
+    @GetMapping("/index")
     public String register() {
         return "progreen/index";
     }
